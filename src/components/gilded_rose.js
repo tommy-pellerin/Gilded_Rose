0311ca6300e27e33fbc0e4d1e17251f1f92171ef
tommy-pellerin <tommytp@hotmail.fr>
2024-05-29 14:44:34 +0200

    gilded_rose refactoring

--- src/components/gilded_rose.js
+++ src/components/gilded_rose.js
@@ -13,13 +13,25 @@ export class Shop {
   decreaseQualityBy1(i){
     return this.items[i].quality = this.items[i].quality - 1;
   }
-  decreaseQualityBy1ExceptSulfuras(i){
-    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') { //if is not sulfuras, deccrease by 1/day => condition to not touch sulfuras quality
+  decreaseQuality(i){
+    if (this.items[i].quality > 0) { //condition to decrease only if quality > 0 (quality min = 0) and if is not 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert'
       this.decreaseQualityBy1(i);
+      this.ifConjuredItem(i);
     }
   }
+  // decreaseQualityBy1ExceptSulfuras(i){
+  //   if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') { //if is not sulfuras, deccrease by 1/day => condition to not touch sulfuras quality
+  //     this.decreaseQualityBy1(i);
+  //   }
+  // }
   increaseQualityBy1(i){
     return this.items[i].quality = this.items[i].quality + 1;
+  }
+  increaseQuality(i){
+    if (this.items[i].quality < 50) { //condition to increase only if quality < 50 (quality max = 50) and if is 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert' and quality < 50
+      this.increaseQualityBy1(i);
+      this.ifBackstagePassesToATAFKAL80ETCConcert(i);
+    }
   }
   decreaseSellIn(i){
     this.items[i].sellIn = this.items[i].sellIn - 1;
@@ -45,44 +57,39 @@ export class Shop {
       }
     }
   }
+  putQUalityTo0(i){
+    this.items[i].quality = this.items[i].quality - this.items[i].quality; //put quality to 0 when the concert is over
+  }
   
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') { //if is not 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert'
-        if (this.items[i].quality > 0) { //condition to decrease only if quality > 0 (quality min = 0) and if is not 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert'
-          this.decreaseQualityBy1ExceptSulfuras(i);
-          this.ifConjuredItem(i);
-        }
-      } else {
-        if (this.items[i].quality < 50) { //condition to increase only if quality < 50 (quality max = 50) and if is 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert' and quality < 50
-          this.increaseQualityBy1(i);
-          this.ifBackstagePassesToATAFKAL80ETCConcert(i);
-        }
-      }
+      if(this.items[i].name != 'Sulfuras, Hand of Ragnaros'){
 
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') { //decrease SellIn by 1 for every item except sulfuras
+        if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') { //if is not 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert'
+          this.decreaseQuality(i)
+        } else {
+          this.increaseQuality(i)
+        }
+  
+        //decrease SellIn by 1 for every item except sulfuras
         this.decreaseSellIn(i);
-      }
-
-      if (this.items[i].sellIn < 0) { //Once the sell by date has passed
-        switch(this.items[i].name){
-          case 'Aged Brie': //quanlity of 'Aged Brie increase by 1 again ?
-            if (this.items[i].quality < 50) { //condition to increase only if quality < 50 (quality max = 50)
-              this.increaseQualityBy1(i);
-            }
-            break;
-          case 'Backstage passes to a TAFKAL80ETC concert':
-            this.items[i].quality = this.items[i].quality - this.items[i].quality; //put quality to 0 when the concert is over
-            break;
-          default:
-            if (this.items[i].quality > 0) { //condition to decrease only if quality > 0 (quality min = 0)
-              this.decreaseQualityBy1ExceptSulfuras(i)
-              this.ifConjuredItem(i);
-            }
+  
+        //Once the sell by date has passed
+        if (this.items[i].sellIn < 0) { 
+          switch(this.items[i].name){
+            case 'Aged Brie': //quanlity of 'Aged Brie increase by 1 again ?
+              this.increaseQuality(i)
+              break;
+            case 'Backstage passes to a TAFKAL80ETC concert':
+              this.putQUalityTo0(i)
+              break;
+            default:
+              this.decreaseQuality(i)
+          }//end switch
         }
-      }
 
+      }//end of sulfuras if
     }
 
     return this.items;
--- src/spec/gilded_rose_spec.js
+++ src/spec/gilded_rose_spec.js
@@ -2,31 +2,31 @@ import { Shop, Item } from "../components/gilded_rose.js";
 
 describe("Gilded Rose", function() {
 
-  // it("full test", () => {
-  //   const items = [
-  //     new Item("+5 Dexterity Vest", 10, 20), //reminder : item(name,sellIn,quality)
-  //     new Item("Aged Brie", 2, 0),
-  //     new Item("Elixir of the Mongoose", 5, 7),
-  //     new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-  //     new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  //     new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  //     new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  //     new Item("Backstage passes to a TAFKAL80ETC concert", 5, 39),
+  it("full test", () => {
+    const items = [
+      new Item("+5 Dexterity Vest", 10, 20), //reminder : item(name,sellIn,quality)
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 39),
 
-  //     // This Conjured item does not work properly yet
-  //     new Item("Conjured Mana Cake", 3, 6),
-  //   ];
+      // This Conjured item does not work properly yet
+      new Item("Conjured Mana Cake", 3, 6),
+    ];
 
-  //   const days = Number(process.argv[2]) || 2; //process.argv[2] vu avec les algorithmes pour lire le troisième argument que passé à au script. ex: si on lance pnpm run test 10, alors process.argv[2] = 10
-  //   const gildedRose = new Shop(items);
+    const days = Number(process.argv[2]) || 7; //process.argv[2] vu avec les algorithmes pour lire le troisième argument que passé à au script. ex: si on lance pnpm run test 10, alors process.argv[2] = 10
+    const gildedRose = new Shop(items);
 
-  //   for (let day = 0; day < days; day++) {
-  //     console.log(`\n-------- day ${day} --------`);
-  //     console.log("name, sellIn, quality");
-  //     items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
-  //     gildedRose.updateQuality();
-  //   }
-  // });
+    for (let day = 0; day < days; day++) {
+      console.log(`\n-------- day ${day} --------`);
+      console.log("name, sellIn, quality");
+      items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
+      gildedRose.updateQuality();
+    }
+  });
 
     //1.Test if "Backstage passes" quality increase by 3 when SellIn < 6
     it("'Backstage passes' quality should increase by 3 when SellIn < 6", function() {
