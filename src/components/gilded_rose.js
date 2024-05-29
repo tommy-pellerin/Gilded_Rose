8106a1e002ea1388af4c8f54a5768bec7acf2cb5
tommy-pellerin <tommytp@hotmail.fr>
2024-05-29 10:30:39 +0200

    improve updateQUality

--- src/components/gilded_rose.js
+++ src/components/gilded_rose.js
@@ -13,6 +13,11 @@ export class Shop {
   decreaseQualityBy1(i){
     return this.items[i].quality = this.items[i].quality - 1;
   }
+  decreaseQualityBy1ExceptSulfuras(i){
+    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') { //if is not sulfuras, deccrease by 1/day => condition to not touch sulfuras quality
+      this.decreaseQualityBy1(i);
+    }
+  }
   increaseQualityBy1(i){
     return this.items[i].quality = this.items[i].quality + 1;
   }
@@ -37,12 +42,10 @@ export class Shop {
   
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-
+      
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') { //if is not 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert'
         if (this.items[i].quality > 0) { //condition to decrease only if quality > 0 (quality min = 0) and if is not 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert'
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') { //if is not sulfuras, deccrease by 1/day => condition to not touch sulfuras quality
-            this.decreaseQualityBy1(i);
-          }
+          this.decreaseQualityBy1ExceptSulfuras(i)
         }
       } else {
         if (this.items[i].quality < 50) { //condition to increase only if quality < 50 (quality max = 50) and if is 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert' and quality < 50
@@ -55,21 +58,20 @@ export class Shop {
         this.decreaseSellIn(i);
       }
 
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') { //all exept 'aged brie'
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') { 
-            if (this.items[i].quality > 0) { //condition to decrease only if quality > 0 (quality min = 0)
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') { //if is not sulfuras, deccrease by 1/day => condition to not touch sulfuras quality
-                this.decreaseQualityBy1(i);
-              }
+      if (this.items[i].sellIn < 0) { //Once the sell by date has passed
+        switch(this.items[i].name){
+          case 'Aged Brie':
+            if (this.items[i].quality < 50) { //condition to increase only if quality < 50 (quality max = 50)
+              this.increaseQualityBy1(i);
             }
-          } else { //if is "backstage passes"
+            break;
+          case 'Backstage passes to a TAFKAL80ETC concert':
             this.items[i].quality = this.items[i].quality - this.items[i].quality; //put quality to 0 when the concert is over
-          }
-        } else { //if is "aged brie"
-          if (this.items[i].quality < 50) { //condition to increase only if quality < 50 (quality max = 50)
-            this.increaseQualityBy1(i);
-          }
+            break;
+          default:
+            if (this.items[i].quality > 0) { //condition to decrease only if quality > 0 (quality min = 0)
+              this.decreaseQualityBy1ExceptSulfuras(i)
+            }
         }
       }
 
